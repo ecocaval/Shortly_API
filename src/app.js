1c54cb888ff1e1ea37d6ad316baaa282456cf76b
erico <ecocaval@hotmail.com>
2023-02-24 13:07:20 -0300

    adding feature: GET /ranking

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import cors from "cors"
 import { userRoutes } from "./routes/usersRoutes/userRoutes.js";
 import { urlsRoutes } from "./routes/urlsRoutes/urlsRoutes.js";
 import { signRoutes } from "./routes/signRoutes/signRoutes.js";
+import { rankingRoutes } from "./routes/rankingRoutes/rankingRoutes.js";
 
 const app = express()
 
@@ -15,6 +16,7 @@ app.use(express.json())
 app.use(userRoutes)
 app.use(signRoutes)
 app.use(urlsRoutes)
+app.use(rankingRoutes)
 
 app.listen(process.env.PORT, () => {
     console.log("listening on port " + process.env.PORT)
