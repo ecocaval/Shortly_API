703c5b6650d1c777bc91ac4600551ef9ce86a305
erico <ecocaval@hotmail.com>
2023-02-24 12:14:35 -0300

    adding feature: GET /users/me

---
+++ src/controllers/usersController/usersController.js
@@ -0,0 +1,32 @@
+import { db } from "../../config/database.connection.js";
+
+export async function getMyUser(req, res) {
+    const userId = req.userId 
+    try {
+
+        const response = await db.query(
+            `
+            SELECT 
+                users.id, 
+                users.name, 
+                SUM(urls.visit_count),
+                JSON_AGG(JSON_BUILD_OBJECT(
+                    'id', urls.id,
+                    'shortUrl', urls.short_url,
+                    'url', urls.url,
+                    'visitCount', urls.visit_count
+                  )) as "shortenedUrls"           
+            FROM users
+            LEFT JOIN urls ON users.id = urls.user_id            
+            WHERE users.id = $1
+            GROUP BY users.id;
+            `,
+            [userId]
+        )        
+        return res.send(response.rows[0])
+
+    } catch (err) {
+        console.log(err)
+        return res.sendStatus(500)
+    }
+}
--- src/routes/usersRoutes/userRoutes.js
+++ src/routes/usersRoutes/userRoutes.js
@@ -1,13 +1,17 @@
 import { Router } from "express";
 import { signInUser, signUpUser } from "../../controllers/signController/signController.js";
+import { getMyUser } from "../../controllers/usersController/usersController.js";
 import createToken from "../../middlewares/userRoutes/createToken.js";
 import verifyRepeatedEmail from "../../middlewares/userRoutes/verifyRepeatedEmail.js";
 import verifyRepeatedToken from "../../middlewares/userRoutes/verifyRepeatedToken.js";
+import { validateToken } from "../../middlewares/validateToken.js";
 import { verifySchema } from "../../middlewares/verifySchema.js";
 import { signInSchema } from "../../schemas/signInSchema.js";
 import { signUpSchema } from "../../schemas/signUpSchema.js";
 
 export const userRoutes = Router();
+
+userRoutes.get("/users/me", validateToken, getMyUser);
 
 userRoutes.post("/signup", verifySchema(signUpSchema), verifyRepeatedEmail, signUpUser);
 
